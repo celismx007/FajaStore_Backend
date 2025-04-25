f146e783d4d4aca079c3a58d48a16ce7626b65f9
celismx007 <tapiacolque7@gmail.com>
2025-04-25 18:17:08 -0400

    configurando las conxion a la BD

--- database/connection.js
+++ database/connection.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     { 
         host: process.env.DB_HOST, 
-        port: 3306,
+        port: process.env.DB_PORT,
         dialect: process.env.DB_DIALECT, 
         define: {
             underscored: true
